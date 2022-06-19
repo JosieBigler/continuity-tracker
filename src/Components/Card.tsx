566b18368a4cc89ab6bbf78382f7ec0caebe0c4b
shawn <shawnmmck@gmail.com>
2022-06-19 10:00:53 -0500

    Unique ID key error and some CSS

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -12,14 +12,12 @@ class Card extends Component<CardProps> {
   render() {
     const { title, subtext: date, id   } = this.props;
     return (
-      <>
       <div className='card'>
         <div className='container'>
           <h4>{title}</h4>
           <p>{date}</p>
         </div>
       </div>
-      </>
     );
   }
 }
